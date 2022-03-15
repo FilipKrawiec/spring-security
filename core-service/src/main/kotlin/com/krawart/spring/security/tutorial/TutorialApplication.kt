820aa5dde8930d60ad94e38bb4587cf2b6f45fc1
Filip Krawiec <filip.krawiec@3pillarglobal.com>
2022-03-15 18:57:57 +0100

    Initial commit

---
+++ core-service/src/main/kotlin/com/krawart/spring/security/tutorial/TutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.krawart.spring.security.tutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TutorialApplication
+
+fun main(args: Array<String>) {
+    runApplication<TutorialApplication>(*args)
+}
