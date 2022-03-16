ef13db1811e69344b3042ea6885926e92d705fe8
Filip Krawiec <filip.krawiec@3pillarglobal.com>
2022-03-17 00:09:46 +0100

    feat[02] Fixed wrong auth controller impl

--- core-service/src/main/kotlin/com/krawart/spring/security/tutorial/identityaccess/presentation/rest/AuthController.kt
+++ core-service/src/main/kotlin/com/krawart/spring/security/tutorial/identityaccess/presentation/rest/AuthController.kt
@@ -3,48 +3,44 @@ package com.krawart.spring.security.tutorial.identityaccess.presentation.rest
 import com.krawart.spring.security.tutorial.identityaccess.application.UserService
 import com.krawart.spring.security.tutorial.identityaccess.application.command.AddUserCommand
 import com.krawart.spring.security.tutorial.identityaccess.domain.exception.EmailAlreadyUsedException
-import com.krawart.spring.security.tutorial.identityaccess.domain.exception.InvalidPasswordConfirmationException
 import com.krawart.spring.security.tutorial.identityaccess.presentation.rest.requestbody.RegisterUserRequestBody
-import org.springframework.stereotype.Controller
 import org.springframework.validation.BindingResult
 import org.springframework.validation.FieldError
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.ModelAndView
 import javax.validation.Valid
 
-@Controller
+@RestController
 @RequestMapping("/")
 class AuthController(
     val userService: UserService
 ) {
 
     @GetMapping("login")
-    fun loginPage(): String = "loginPage"
+    fun loginPage(): ModelAndView = ModelAndView("loginPage")
 
     @GetMapping("sign-up")
-    fun registrationPage(): ModelAndView = ModelAndView("registrationPage", "user", RegisterUserRequestBody())
+    fun registrationPage(): ModelAndView = ModelAndView("registrationPage", "body", RegisterUserRequestBody())
 
     @PostMapping("user/register")
-    fun registerUser(@Valid body: RegisterUserRequestBody, result: BindingResult): ModelAndView {
-        if (result.hasErrors()) return ModelAndView("registrationPage", "user", body)
+    fun registerUser(
+        @Valid @ModelAttribute("body") body: RegisterUserRequestBody,
+        result: BindingResult
+    ): ModelAndView {
+        if (result.hasErrors()) return ModelAndView("registrationPage", "body", body)
 
-        validatePasswordConfirmation(body)
+        if (body.password != body.passwordConfirmation) {
+            result.addError(FieldError("body", "passwordConfirmation", "Password confirmation is not matching"))
+            return ModelAndView("registrationPage", "body", body)
+        }
 
         try {
             userService.addUser(AddUserCommand(body.email, body.password))
         } catch (e: EmailAlreadyUsedException) {
-            result.addError(FieldError("user", "email", e.message))
-            return ModelAndView("registrationPage", "user", body)
+            result.addError(FieldError("body", "email", e.message))
+            return ModelAndView("registrationPage", "body", body)
         }
 
         return ModelAndView("redirect:/login")
-    }
-
-    private fun validatePasswordConfirmation(body: RegisterUserRequestBody) {
-        if (body.password != body.passwordConfirmation) {
-            throw InvalidPasswordConfirmationException("Password confirmation must be valid")
-        }
     }
 }
