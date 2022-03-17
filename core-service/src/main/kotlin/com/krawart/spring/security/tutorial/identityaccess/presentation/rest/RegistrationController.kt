087e644659284f60f7935924b4d639ab908230dc
Filip Krawiec <filip.krawiec@3pillarglobal.com>
2022-03-17 10:07:59 +0100

    feat[02] Registration flow put to dedicated controller

---
+++ core-service/src/main/kotlin/com/krawart/spring/security/tutorial/identityaccess/presentation/rest/RegistrationController.kt
@@ -0,0 +1,43 @@
+package com.krawart.spring.security.tutorial.identityaccess.presentation.rest
+
+import com.krawart.spring.security.tutorial.identityaccess.application.UserService
+import com.krawart.spring.security.tutorial.identityaccess.application.command.AddUserCommand
+import com.krawart.spring.security.tutorial.identityaccess.domain.exception.EmailAlreadyUsedException
+import com.krawart.spring.security.tutorial.identityaccess.presentation.rest.requestbody.RegisterUserRequestBody
+import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.ModelAndView
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/")
+class RegistrationController(
+    val userService: UserService
+) {
+
+    @GetMapping("sign-up")
+    fun registrationPage(): ModelAndView = ModelAndView("registrationPage", "body", RegisterUserRequestBody())
+
+    @PostMapping("user/register")
+    fun registerUser(
+        @Valid @ModelAttribute("body") body: RegisterUserRequestBody,
+        result: BindingResult
+    ): ModelAndView {
+        if (result.hasErrors()) return ModelAndView("registrationPage", "body", body)
+
+        if (body.password != body.passwordConfirmation) {
+            result.addError(FieldError("body", "passwordConfirmation", "Password confirmation is not matching"))
+            return ModelAndView("registrationPage", "body", body)
+        }
+
+        try {
+            userService.addUser(AddUserCommand(body.email, body.password))
+        } catch (e: EmailAlreadyUsedException) {
+            result.addError(FieldError("body", "email", e.message))
+            return ModelAndView("registrationPage", "body", body)
+        }
+
+        return ModelAndView("redirect:/login")
+    }
+}
